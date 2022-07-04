a8080ed6f44262538f9758665374a9552a4d50c0
richardd82 <richardd82@gmail.com>
2022-07-04 04:30:58 -0500

    fix space °C

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,23 +4,23 @@ import Card from './Card';
 import '../App.js';
 import styles from '../styles/Card.module.css';
 export default function Cards({cities, onClose}) {
-  
+
   if(cities){
     console.log(cities)
     return (
     <div className={styles.Cards}>
       {
-        cities.map(cities => 
-          <Card           
-            max={cities.max} 
-            min={cities.min} 
-            name={cities.name} 
-            img={cities.img} 
-            onClose={()=> onClose(cities.id)} 
+        cities.map(cities =>
+          <Card
+            max={cities.max + " °C"}
+            min={cities.min + " °C"} 
+            name={cities.name}
+            img={cities.img}
+            onClose={()=> onClose(cities.id)}
             key={cities.id}
             id={cities.id}
-          />          
-        )        
+          />
+        )
       }
     </div>
     )
