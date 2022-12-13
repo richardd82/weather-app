9ad13ab2e49ca3177dd68a89fa257e619f028365
Richardd82 <richardd82@gmail.com>
2022-12-13 01:31:31 -0600

    Fix < 2

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import styles from '../styles/Card.module.css';
-export default function Card({max, min, name, img, onClose}) {
+import React from "react";
+import styles from "../styles/Card.module.css";
+export default function Card({ max, min, name, img, onClose }) {
   //console.log(arr)
-  return <div className={styles.Card}>
-    <button className={styles.Btn} onClick={onClose}>X</button>
-    <h3>Max</h3>
-    <p>{max} °C</p>
-    <h3>Min</h3>
-    <p>{min} °C</p>
-    <img src={img} alt=""/>
-    <h2>{name}</h2>
-  </div>
-  };
+  return (
+    <div className={styles.Card}>
+      <button className={styles.Btn} onClick={onClose}>
+        X
+      </button>
+      <h3>Max</h3>
+      <p>{max} °C</p>
+      <h3>Min</h3>
+      <p>{min} °C</p>
+      <img src={img} alt="" />
+      <h2>{name}</h2>
+    </div>
+  );
+}
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,22 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 //import '../App.js';
-import styles from '../styles/Card.module.css';
+import styles from "../styles/Card.module.css";
 
-export default function SearchBar({onSearch}) {
+export default function SearchBar({ onSearch }) {
   const [city, setCity] = useState("");
-  return (<div className={styles.SearchBar} >
-    <form onSubmit={(e) => {
-      e.preventDefault();
-      onSearch(city);
-      setCity("");
-    }}>
-    <input 
-      className={styles.TextSearch} 
-      placeholder='Ciudad...' 
-      value = {city}
-      onChange = {e => setCity(e.target.value)}
-    /> 
-    <input className={styles.Button} type="submit" value="agregar" />
-    </form>
-  </div>)
-};
+  return (
+    <div className={styles.SearchBar}>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          onSearch(city);
+          setCity("");
+        }}
+      >
+        <input
+          className={styles.TextSearch}
+          placeholder="Ciudad..."
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <input className={styles.Button} type="submit" value="agregar" />
+      </form>
+    </div>
+  );
+}
